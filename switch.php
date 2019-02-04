14fbfcc698d06a501f1aaf17c1857f460b43c6d6
jyththeman <jythgerundio@gmail.com>
2019-02-04 15:39:59 +0800

    final commit

---
+++ switch.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <script>
+function red() {
+  document.body.style.backgroundColor = "red";
+}
+    function blue() {
+  document.body.style.backgroundColor = "blue";
+}
+    function green() {
+  document.body.style.backgroundColor = "green";
+}
+</script>
+
+<body>
+
+<h1>Please Input Color</h1>
+
+    
+
+<button type="button" onclick="red()">Set background color</button>
+<button type="button" onclick="blue()">Set background color</button>
+<button type="button" onclick="green()">Set background color</button>
+
+</body>
+</html>
